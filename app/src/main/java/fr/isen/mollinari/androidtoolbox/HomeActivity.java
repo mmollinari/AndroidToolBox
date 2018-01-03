770c14a8ab96e06462af0f0c7414259f8cb0b769
Marc MOLLINARI <marc.mollinari@it-ce.fr>
2018-01-03 11:57:50 +0100

    Ajout de la page Home

---
+++ app/src/main/java/fr/isen/mollinari/androidtoolbox/HomeActivity.java
@@ -0,0 +1,30 @@
+package fr.isen.mollinari.androidtoolbox;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        String identifiant = getIntent().getStringExtra("strIdentifiantadmin");
+        String message = getString(R.string.home_welcome) + " : \n" + identifiant;
+        ((TextView)findViewById(R.id.tvBonjour)).setText(message);
+
+        findViewById(R.id.ivCdv).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(HomeActivity.this, HomeActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/fr/isen/mollinari/androidtoolbox/LoginActivity.java
+++ app/src/main/java/fr/isen/mollinari/androidtoolbox/LoginActivity.java
@@ -1,7 +1,12 @@
 package fr.isen.mollinari.androidtoolbox;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnValider = findViewById(R.id.button);
+        final EditText etIdentifiant = findViewById(R.id.editTextIdentifiant);
+        final EditText etMdp = findViewById(R.id.editTextMdp);
+
+        btnValider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String identifiant = etIdentifiant.getText().toString();
+                String mdp = etMdp.getText().toString();
+                if ("admin".equals(identifiant) && "123".equals(mdp)) {
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    intent.putExtra("strIdentifiant", identifiant);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Identifiant ou mot de passe incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
